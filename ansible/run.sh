472c62ab0ce8fef6efa4c8a177aa6208e43c425c
Marc Wimmer <marc@itewimmer.de>
2021-03-27 22:25:20 +0100

    ansible: jenkins job visible

--- ansible/run.sh
+++ ansible/run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-ansible-galaxy install emmetog.jenkins
+# ansible-galaxy install emmetog.jenkins
 
 cd ansible || exit -1
-ansible-playbook -i data/hosts cicd.yml
+ansible-playbook -i data/hosts cicd.yml -vvvv
